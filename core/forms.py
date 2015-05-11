4e215a7b7c94d554ba830a4876bd5930eaeaba7f
Сергей <novoseletskiyserhiy@gmail.com>
2015-05-11 18:33:16 +0300

    v 1.0.1

--- core/forms.py
+++ core/forms.py
@@ -7,28 +7,34 @@ from core.models import Distinct, City, Place, Owner, Marchent, Container
 class DistinctForm(forms.ModelForm):
     class Meta:
         model = Distinct
+        fields = ['name', 'region']
 
 
 class CityForm(forms.ModelForm):
     class Meta:
         model = City
+        fields = ['name', 'distinct']
 
 
 class PlaceForm(forms.ModelForm):
     class Meta:
         model = Place
+        fields = ['name', 'city']
 
 
 class OwnerForm(forms.ModelForm):
     class Meta:
         model = Owner
+        exclude = []
 
 
 class MarchentForm(forms.ModelForm):
     class Meta:
         model = Marchent
+        exclude = []
 
 
 class ContainerForm(forms.ModelForm):
     class Meta:
         model = Container
+        exclude = []
--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -17,23 +17,24 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=70, verbose_name=b'\xd0\x9d\xd0\xb0\xd0\xb7\xd0\xb2\xd0\xb0')),
             ],
             options={
+                'ordering': ['-id'],
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Container',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('weight', models.FloatField(verbose_name=b'\xd0\xa2\xd0\xbe\xd0\xbd\xd0\xbd\xd0\xb0\xd0\xb6')),
+                ('name', models.CharField(max_length=100, verbose_name=b'\xd0\x9d\xd0\xb0\xd0\xb7\xd0\xb2\xd0\xb0')),
+                ('weight', models.IntegerField(verbose_name=b'\xd0\xa2\xd0\xbe\xd0\xbd\xd0\xbd\xd0\xb0\xd0\xb6')),
                 ('row', models.IntegerField(verbose_name=b'\xd0\xa0\xd1\x8f\xd0\xb4')),
                 ('rent', models.CharField(max_length=255, verbose_name=b'\xd0\x9e\xd1\x80\xd0\xb5\xd0\xbd\xd0\xb4\xd0\xb0')),
                 ('contract_begin', models.DateField(verbose_name=b'\xd0\x9f\xd0\xbe\xd1\x87\xd0\xb0\xd1\x82\xd0\xbe\xd0\xba \xd0\xbe\xd1\x80\xd0\xb5\xd0\xbd\xd0\xb4\xd0\xb8')),
                 ('contract_end', models.DateField(verbose_name=b'\xd0\x9a\xd1\x96\xd0\xbd\xd0\xb5\xd1\x86\xd1\x8c \xd0\xbe\xd1\x80\xd0\xb5\xd0\xbd\xd0\xb4\xd0\xb8')),
-                ('description', models.TextField(verbose_name=b'\xd0\x94\xd0\xbe\xd0\xb4\xd0\xb0\xd1\x82\xd0\xba\xd0\xbe\xd0\xb2\xd0\xb0 \xd1\x96\xd0\xbd\xd1\x84\xd0\xbe\xd1\x80\xd0\xbc\xd0\xb0\xd1\x86\xd1\x96\xd1\x8f')),
+                ('description', models.TextField(verbose_name=b'\xd0\x94\xd0\xbe\xd0\xb4\xd0\xb0\xd1\x82\xd0\xba\xd0\xbe\xd0\xb2\xd0\xb0 \xd1\x96\xd0\xbd\xd1\x84\xd0\xbe\xd1\x80\xd0\xbc\xd0\xb0\xd1\x86\xd1\x96\xd1\x8f', blank=True)),
             ],
             options={
+                'ordering': ['-id'],
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Distinct',
@@ -42,42 +43,43 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=70, verbose_name=b'\xd0\x9d\xd0\xb0\xd0\xb7\xd0\xb2\xd0\xb0')),
             ],
             options={
+                'ordering': ['-id'],
+                'verbose_name': '\u0420\u0430\u0439\u043e\u043d',
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Marchent',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('first_name', models.CharField(max_length=70, verbose_name=b'\xd0\x86\xd0\xbc`\xd1\x8f')),
-                ('last_name', models.CharField(max_length=70, verbose_name=b'\xd0\x9f\xd1\x80\xd1\x96\xd0\xb7\xd0\xb2\xd0\xb8\xd1\x89\xd0\xb5')),
-                ('sur_name', models.CharField(max_length=70, verbose_name=b'\xd0\x9f\xd0\xbe-\xd0\x91\xd0\xb0\xd1\x82\xd1\x8c\xd0\xba\xd0\xbe\xd0\xb2\xd1\x96')),
+                ('last_name', models.CharField(max_length=70, verbose_name=b'\xd0\x9f\xd0\xbe-\xd0\x91\xd0\xb0\xd1\x82\xd1\x8c\xd0\xba\xd0\xbe\xd0\xb2\xd1\x96')),
+                ('sur_name', models.CharField(max_length=70, verbose_name=b'\xd0\x9f\xd1\x80\xd1\x96\xd0\xb7\xd0\xb2\xd0\xb8\xd1\x89\xd0\xb5')),
                 ('telephone', models.CharField(max_length=60, verbose_name=b'\xd0\xa2\xd0\xb5\xd0\xbb\xd0\xb5\xd1\x84\xd0\xbe\xd0\xbd')),
                 ('email', models.EmailField(max_length=150, verbose_name=b'Email')),
                 ('address', models.CharField(max_length=150, verbose_name=b'\xd0\x90\xd0\xb4\xd1\x80\xd0\xb5\xd1\x81')),
-                ('description', models.TextField(verbose_name=b'\xd0\x94\xd0\xbe\xd0\xb4\xd0\xb0\xd1\x82\xd0\xba\xd0\xbe\xd0\xb2\xd0\xb0 \xd1\x96\xd0\xbd\xd1\x84\xd0\xbe\xd1\x80\xd0\xbc\xd0\xb0\xd1\x86\xd1\x96\xd1\x8f')),
+                ('description', models.TextField(verbose_name=b'\xd0\x94\xd0\xbe\xd0\xb4\xd0\xb0\xd1\x82\xd0\xba\xd0\xbe\xd0\xb2\xd0\xb0 \xd1\x96\xd0\xbd\xd1\x84\xd0\xbe\xd1\x80\xd0\xbc\xd0\xb0\xd1\x86\xd1\x96\xd1\x8f', blank=True)),
                 ('city', models.ForeignKey(verbose_name=b'\xd0\x9c\xd1\x96\xd1\x81\xd1\x82\xd0\xbe', to='core.City')),
             ],
             options={
+                'ordering': ['-id'],
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Owner',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('first_name', models.CharField(max_length=70, verbose_name=b'\xd0\x86\xd0\xbc`\xd1\x8f')),
-                ('last_name', models.CharField(max_length=70, verbose_name=b'\xd0\x9f\xd1\x80\xd1\x96\xd0\xb7\xd0\xb2\xd0\xb8\xd1\x89\xd0\xb5')),
-                ('sur_name', models.CharField(max_length=70, verbose_name=b'\xd0\x9f\xd0\xbe-\xd0\x91\xd0\xb0\xd1\x82\xd1\x8c\xd0\xba\xd0\xbe\xd0\xb2\xd1\x96')),
+                ('last_name', models.CharField(max_length=70, verbose_name=b'\xd0\x9f\xd0\xbe-\xd0\x91\xd0\xb0\xd1\x82\xd1\x8c\xd0\xba\xd0\xbe\xd0\xb2\xd1\x96')),
+                ('sur_name', models.CharField(max_length=70, verbose_name=b'\xd0\x9f\xd1\x80\xd1\x96\xd0\xb7\xd0\xb2\xd0\xb8\xd1\x89\xd0\xb5')),
                 ('telephone', models.CharField(max_length=60, verbose_name=b'\xd0\xa2\xd0\xb5\xd0\xbb\xd0\xb5\xd1\x84\xd0\xbe\xd0\xbd')),
                 ('email', models.EmailField(max_length=150, verbose_name=b'Email')),
                 ('address', models.CharField(max_length=150, verbose_name=b'\xd0\x90\xd0\xb4\xd1\x80\xd0\xb5\xd1\x81')),
-                ('description', models.TextField(verbose_name=b'\xd0\x94\xd0\xbe\xd0\xb4\xd0\xb0\xd1\x82\xd0\xba\xd0\xbe\xd0\xb2\xd0\xb0 \xd1\x96\xd0\xbd\xd1\x84\xd0\xbe\xd1\x80\xd0\xbc\xd0\xb0\xd1\x86\xd1\x96\xd1\x8f')),
+                ('description', models.TextField(verbose_name=b'\xd0\x94\xd0\xbe\xd0\xb4\xd0\xb0\xd1\x82\xd0\xba\xd0\xbe\xd0\xb2\xd0\xb0 \xd1\x96\xd0\xbd\xd1\x84\xd0\xbe\xd1\x80\xd0\xbc\xd0\xb0\xd1\x86\xd1\x96\xd1\x8f', blank=True)),
                 ('city', models.ForeignKey(verbose_name=b'\xd0\x9c\xd1\x96\xd1\x81\xd1\x82\xd0\xbe', to='core.City')),
             ],
             options={
+                'ordering': ['-id'],
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Place',
@@ -87,8 +89,8 @@ class Migration(migrations.Migration):
                 ('city', models.ForeignKey(verbose_name=b'\xd0\x9c\xd1\x96\xd1\x81\xd1\x82\xd0\xbe', to='core.City')),
             ],
             options={
+                'ordering': ['-id'],
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Region',
@@ -97,8 +99,8 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=70, verbose_name=b'\xd0\x9d\xd0\xb0\xd0\xb7\xd0\xb2\xd0\xb0')),
             ],
             options={
+                'ordering': ['-id'],
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Type',
@@ -107,43 +109,37 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=70, verbose_name=b'\xd0\x9d\xd0\xb0\xd0\xb7\xd0\xb2\xd0\xb0')),
             ],
             options={
+                'ordering': ['-id'],
             },
-            bases=(models.Model,),
         ),
         migrations.AddField(
             model_name='distinct',
             name='region',
             field=models.ForeignKey(verbose_name=b'\xd0\xa0\xd0\xb5\xd0\xb3\xd1\x96\xd0\xbe\xd0\xbd', to='core.Region'),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='container',
             name='marchent',
             field=models.ForeignKey(verbose_name=b'\xd0\x9e\xd1\x80\xd0\xb5\xd0\xbd\xd0\xb4\xd0\xb0\xd1\x80\xd1\x8c', to='core.Marchent'),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='container',
             name='owner',
             field=models.ForeignKey(verbose_name=b'\xd0\x92\xd0\xbb\xd0\xb0\xd1\x81\xd0\xbd\xd0\xb8\xd0\xba', to='core.Owner'),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='container',
             name='place',
             field=models.ForeignKey(verbose_name=b'\xd0\x9c\xd1\x96\xd1\x81\xd1\x86\xd0\xb5', to='core.Place'),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='container',
             name='type',
             field=models.ForeignKey(verbose_name=b'\xd0\xa2\xd0\xb8\xd0\xbf', to='core.Type'),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='city',
             name='distinct',
             field=models.ForeignKey(verbose_name=b'\xd0\xa0\xd0\xb0\xd0\xb9\xd0\xbe\xd0\xbd', to='core.Distinct'),
-            preserve_default=True,
         ),
     ]
